333eac074d7c4051d61d55d837dfe8eabf77ce54
malfians <piansaputra322@gmail.com>
2017-02-27 12:01:27 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas3.java
@@ -0,0 +1,14 @@
+public class tugas3{
+
+     public static void main(String []args){
+        int a = 10;
+        int b = TampilAngka(a);
+        System.out.println("Angka yang anda masukkan adalah angka " + b);
+     }
+     
+     public static int TampilAngka(int aa) {
+      int angka = aa;
+      
+      return angka; 
+   }
+}
