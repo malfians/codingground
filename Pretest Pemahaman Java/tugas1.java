40c06ceaadb226eb0e0d45b4d9223dec8f9946a0
malfians <piansaputra322@gmail.com>
2017-02-27 12:03:33 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas1.java
@@ -0,0 +1,9 @@
+public class tugas1{
+
+     public static void main(String []args){
+         String nama = "Alfian";
+         String kota = "Jepara";
+         
+        System.out.println("Nama : " + nama + " kota : " +kota);
+     }
+}
