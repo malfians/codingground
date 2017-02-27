7d901bcb4627971eb95c71e27e4e9e16921f6823
malfians <piansaputra322@gmail.com>
2017-02-27 12:08:38 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas5.java
@@ -0,0 +1,17 @@
+public class tugas5{
+
+     public static void main(String []args){
+        int a = 8;
+        int hasil = HitungMundur(a);
+        
+        for (int i = hasil; i>= 1; i--) {
+         System.out.println(i);
+        }
+     }
+     
+     public static int HitungMundur(int aa) {
+      int angka = aa;
+      
+      return angka; 
+     }
+}
