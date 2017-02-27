9c68a9fd27588d190350ea0bc178d1b4f3d428da
malfians <piansaputra322@gmail.com>
2017-02-27 12:06:22 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/tugas4.java
@@ -0,0 +1,16 @@
+public class tugas4{
+
+     public static void main(String []args){
+         
+        int pmeter = 1;
+        int hasil = Penjumlahan(pmeter);
+        
+        System.out.println("Angka penjumlahan " + pmeter + " + 2 = " + hasil);
+     }
+     
+     public static int Penjumlahan(int aa) {
+      int angka = aa;
+
+      return angka + 2; 
+   }
+}
